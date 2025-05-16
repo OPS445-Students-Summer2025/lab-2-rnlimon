bc49343e4053a10a92121db528d5dbfbfd4e534b
Raffy Limon <rnlimon@myseneca.ca>
2025-05-16 00:20:43 -0400

    Added lab2c

---
+++ lab2c.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+# Using arguments
+import sys
+
+# Enter info for name and age as an arguments
+name = sys.argv[1]
+age = sys.argv[2]
+
+# Print the output based on the name and age arguments
+print('Hi ' + name + ', you are ' + str(age) + ' years old.')
