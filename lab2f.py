aae2f7d192396b37f86b882ef447cf234043abe0
Raffy Limon <rnlimon@myseneca.ca>
2025-05-21 21:39:20 -0400

    Added lab2f

---
+++ lab2f.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+# Author: Raffy Limon
+# Author ID: rnlimon
+# Date Created: 2025/05/21
+
+#this is from lab2e
+
+# Interger object
+#timer = 10
+
+# Repeats until timer equals 0
+#while timer != 0:
+#    print(timer)
+#   timer = timer - 1
+#print('blast off!')
+
+#lab2f
+
+import sys
+
+timer = int(sys.argv[1])
+
+# while loop that repeats until timer equals 0
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+print('blast off!')
