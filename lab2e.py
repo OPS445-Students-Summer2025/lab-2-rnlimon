33b2bdbb4eb38c11f1f010b4c71da29e1006e058
Raffy Limon <rnlimon@myseneca.ca>
2025-05-21 21:16:20 -0400

    Added lab2e

---
+++ lab2e.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+# Author: Raffy Limon
+# Author ID: rnlimon
+# Date Created: 2025/05/21
+
+# Interger object
+timer = 10
+
+# Repeats until timer equals 0
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+print('blast off!')
